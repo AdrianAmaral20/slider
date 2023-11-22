d68485dd8930a9081c4130cb06661c9c1d754348
AdrianAmaral20 <adri13amaral@gmail.com>
2023-11-22 13:23:58 -0300

    slide atualizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,9 +23,10 @@ function goNext() {
 }
 
 function updateMargin() {
-  let newMargin = (currentSlides * document.body.clientWidth);
+  let sliderItemWidth = document.querySelector('.slider--item').clientWidth;
+  let newMargin = (currentSlides * sliderItemWidth);
   document.querySelector('.slider--width').style.marginLeft =
   `-${newMargin}px`;
 }
 
-setInterval(goNext, 4000);
+setInterval(goNext, 5000);
