aacae01fa67612bda6d0674dbe1251d6241b2e83
AdrianAmaral20 <adri13amaral@gmail.com>
2023-11-22 12:11:52 -0300

    criado 2 funções para mover as imagens do slide

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+let totalSlides = document.querySelectorAll('.slider--item').length;
+let currentSlides = 0;
+
+document.querySelector('.slider--width').style.width = 
+  `calc(100vw * ${totalSlides})`;
+document.querySelector('.slider--controls').style.height = 
+  `${document.querySelector('.slider').clientHeight}px`;
+
+function goPrev() {
+  currentSlides--;
+  if(currentSlides < 0) {
+    currentSlides = totalSlides - 1;
+  }
+  updateMargin();
+}
+
+function goNext() {
+  currentSlides++;
+  if(currentSlides > (totalSlides - 1)) {
+    currentSlides = 0;
+  }
+  updateMargin();
+}
+
+function updateMargin() {
+
+}
